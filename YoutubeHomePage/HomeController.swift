8523694be96a727df0fc381e3e47ecb36a42d911
Tim Beals <tim.beals@gmail.com>
2017-01-23 13:47:04 -0800

    implement basic collection view setup

---
+++ YoutubeHomePage/HomeController.swift
@@ -0,0 +1,39 @@
+//
+//  HomeController.swift
+//  YoutubeHomePage
+//
+//  Created by Tim Beals on 2017-01-23.
+//  Copyright © 2017 Tim Beals. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "Home"
+        
+        collectionView?.backgroundColor = UIColor.white
+        
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
+        
+
+        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        cell.backgroundColor = UIColor.red
+        return cell
+    }
+
+
+}
+
