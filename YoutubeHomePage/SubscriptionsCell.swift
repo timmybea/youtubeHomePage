5ef908e9477166f3a6521e5b0cf6a9b4554e7fab
Tim Beals <tim.beals@gmail.com>
2017-02-08 12:40:38 -0800

    make async fetch videos calls for each section of the app

---
+++ YoutubeHomePage/SubscriptionsCell.swift
@@ -0,0 +1,21 @@
+//
+//  SubscriptionsCell.swift
+//  YoutubeHomePage
+//
+//  Created by Tim Beals on 2017-02-08.
+//  Copyright © 2017 Tim Beals. All rights reserved.
+//
+
+import UIKit
+
+class SubscriptionsCell: FeedCell {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
